d4463af0b7962e4bf0db540ae1337a784ea05a38
Oliver Ager Wiklander <agerwiklander.oliver@elev.ntig.se>
2023-01-17 13:49:22 +0100

    comments

--- TCPChat/src/Client.java
+++ TCPChat/src/Client.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.Socket;                 //import
 
 
 public class Client implements Runnable, ActionListener {
@@ -18,7 +18,7 @@ public class Client implements Runnable, ActionListener {
     private Socket client;
     private BufferedReader in;
     private PrintWriter out;
-    private boolean done;
+    private boolean done;               //instance variable
 
     @Override
     public void run() {
@@ -37,19 +37,19 @@ public class Client implements Runnable, ActionListener {
             frame.add(panel, BorderLayout.CENTER);
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             frame.pack();
-            frame.setVisible(true);
+            frame.setVisible(true);                         //Creates GUI
             
             client = new Socket("localhost", 9999);
             out = new PrintWriter(client.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));        //Creates input and output between the client and server
 
-            InputHandler inHandler = new InputHandler();
+            InputHandler inHandler = new InputHandler();        //Connects to a thread where the user can communicate with the server
             Thread t = new Thread(inHandler);
-            t.start();
+            t.start();  //Activates thread
 
             String inMessage;
             while((inMessage = in.readLine()) != null) {
-                System.out.println(inMessage);
+                System.out.println(inMessage); //sends message with, out function
             }
         } catch (IOException e) {
             shutdown();
@@ -61,7 +61,7 @@ public class Client implements Runnable, ActionListener {
         try {
             in.close();
             out.close();
-            if(!client.isClosed()) {
+            if(!client.isClosed()) {        //shuts down client
                 client.close();
             }
         } catch (IOException e) {
@@ -80,14 +80,14 @@ public class Client implements Runnable, ActionListener {
         public void run() {
             try {
                 BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
-                while (!done) {
+                while (!done) {  //creates a variable for input by client
                     String message = inReader.readLine();
-                    if (message.equals("/quit")) {
+                    if (message.equals("/quit")) {  //quits program
                         out.println(message);
                         inReader.close();
                         shutdown();
                     } else {
-                        out.println(message);
+                        out.println(message); //sends message
                     }
                 }
             } catch (IOException e) {
@@ -97,7 +97,7 @@ public class Client implements Runnable, ActionListener {
     }
 
     public static void main(String[] args) {
-        Client client = new Client();
+        Client client = new Client(); //runs methods
         client.run();
     }
 }
